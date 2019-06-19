ec396c897f5396975c89fd4f42022237c38b811b
ramshakhan123 <49521382+ramshakhan123@users.noreply.github.com>
2019-06-19 21:46:29 +0500

    #WIP add website and images

---
+++ script.js
@@ -0,0 +1,86 @@
+let initialValue = 0
+let appleValue = 10
+
+/*
+
+    1- jb addOneNum() button ku click krun tu
+    input m +1 value add hute rha jb jb m click krun...
+    
+    2- phr input k value multiply hoge appleValue = 10 se tu
+    woh dono value multiply hukr p span m show hunge...
+         
+
+*/
+function addOneNum() {
+
+    document.querySelector("#add").innerHTML = ++initialValue;
+    let multiply = document.querySelector("#add").innerHTML = +initialValue;
+    document.querySelector("p span").innerHTML = multiply * appleValue;
+    let total1 = document.querySelector("p span").innerHTML = multiply * appleValue;
+
+}
+
+
+let initialValue1 = 0
+let orangeValue = 15
+
+
+
+function add1OneNum() {
+
+    document.querySelector("#add1").innerHTML = ++initialValue1;
+    let multiply1 = document.querySelector("#add1").innerHTML = +initialValue1;
+    let total1 = multiply1 * orangeValue;
+    document.querySelector("p span").innerHTML = total1;
+
+}
+let initialValue2 = 0
+let bananaValue = 7
+
+
+function add2OneNum() {
+
+    document.querySelector("#add2").innerHTML = ++initialValue2;
+    let multiply2 = document.querySelector("#add2").innerHTML = +initialValue2;
+    document.querySelector("p span").innerHTML = multiply2 * bananaValue;
+    let total1 = document.querySelector("p span").innerHTML = multiply2 * bananaValue;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ 
+    jb minusOneNum() button ku click krun tu
+    input m -1 value minus hute rha jb jb m click krun...    
+
+*/
+function minusOneNum() {
+
+   
+   
+    
+    
+
+
+}
+
+
+
+
+
+
+
+
